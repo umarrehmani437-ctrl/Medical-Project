0fbaa7ca1c1a75f5dfe1c3994337891ad3b885bf
Umar Rehmani <umarrehmani437-ctrl@gmail.com>
2025-12-30 12:11:49 +0500

    Initial commit - Dr Hammad Ansari medical website

---
+++ README.md
@@ -0,0 +1,19 @@
+# Responsive Medical Website 
+# Responsive Medical website built using HTML5, CSS3, JavaScript,
+<a href="https://dr-hammadansari.netlify.app/" target="_blank">**Visit Now** 🚀</a>
+### Responsive Medical Website 
+## 📌 Sneak Peek of Main Page 🙈 :
+![preview](/assets/img/preview.png)
+
+- Responsive Medical Website Using HTML CSS JavaScript 
+- Contains animations when scrolling.
+- Smooth scrolling in each section.
+- Real-time map location.
+- Includes a dark & light theme.
+- Developed first with the Mobile First methodology, then for desktop.
+- Compatible with all mobile devices and with a beautiful and pleasant user interface.
+💙 Join the channel to see more videos like this. [UMAR REHMANI](https://umar-rehmani-portfolio.netlify.app/)
+<h2>📬 Contact</h2>
+Feel free to reach me through the below handles if you'd like to contact me.
+[![linkedin](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/umarrehmani-webdeveloper/)
+[![instagram](https://img.shields.io/badge/Instagram-E4405F?style=for-the-badge&logo=instagram&logoColor=white)](https://www.instagram.com/umarrehmani437?igsh=dGNmb2ZzNTFmZnls)
---
+++ assets/js/main.js
@@ -0,0 +1,143 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+/* Menu show */
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* Menu hidden */
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== ADD SHADOW HEADER ===============*/
+const shadowHeader = () => {
+    const header = document.getElementById('header')
+    // Add a class if the bottom offset is greater than 50 of the viewport
+    this.scrollY >= 50
+        ? header.classList.add('shadow-header')
+        : header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll', shadowHeader)
+
+/*=============== SWIPER PRICES ===============*/
+const swiperPrices = new Swiper('.prices__swiper', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 24,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    }
+})
+
+/*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+    this.scrollY >= 350
+        ? scrollUp.classList.add('show-scroll')
+        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () => {
+    const scrollDown = window.scrollY
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add('active-link')
+        } else {
+            sectionsClass.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*=============== DARK LIGHT THEME ===============*/
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-fill'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-fill' : 'ri-sun-fill'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'ri-moon-fill' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    reset: true
+})
+
+sr.reveal(`.home__content`, { origin: 'bottom' })
+sr.reveal(`.home__info`, { origin: 'bottom', delay: 800 })
+sr.reveal(`.home__data`, { delay: 1400 })
+sr.reveal(`.home__button`, { origin: 'left', delay: 1800 })
+
+sr.reveal(`.delivery__data`, { origin: 'right' })
+sr.reveal(`.delivery__content`, { origin: 'left', delay: 600 })
+sr.reveal(`.delivery__img`, { delay: 1200 })
+
+sr.reveal(`.about__data, .contact__map`, { origin: 'left' })
+sr.reveal(`.about__img, .contact__data`, { origin: 'right' })
+
+sr.reveal(`.prices__box`)
+sr.reveal(`.prices__swiper`, { origin: 'bottom', delay: 600 })
+
+sr.reveal(`.gallery__image`, { interval: 200 })
+
+sr.reveal(`.footer__container`)
